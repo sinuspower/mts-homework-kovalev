78c3ec68d4574692ea78e21144da56513cde6a54
OKovalev <OKovalev@mtsbank.ru>
2024-03-17 19:34:50 +0300

    HW-1 Add type and name to AbstractAnimal, use them in childs, move toString() from the leaf classes to its parents

--- src/main/java/ru/mtsb/okovalev/lessonthree/animals/AbstractAnimal.java
+++ src/main/java/ru/mtsb/okovalev/lessonthree/animals/AbstractAnimal.java
@@ -1,11 +1,15 @@
 package ru.mtsb.okovalev.lessonthree.animals;
 
+import ru.mtsb.okovalev.lessonthree.animals.enums.AnimalType;
+
 /**
  * Базовая абстракция животного.
  */
 public abstract class AbstractAnimal implements Animal {
+    protected AnimalType type;
     protected String breed;
     protected String character;
+    protected String name;
 
     /**
      * Задаёт базовые параметры экземпляров дочерних классов.
@@ -13,10 +17,19 @@ public abstract class AbstractAnimal implements Animal {
      * @param breed     Порода животного
      * @param character Поведение (характер) животного
      */
-    public AbstractAnimal(String breed, String character) {
+    public AbstractAnimal(AnimalType type, String breed, String character, String name) {
+        this.type = type;
         this.breed = breed;
         this.character = character;
+        this.name = name;
     }
+
+    /**
+     * Возвращает тип животного. Должен быть имплементирован в классе-наследнике.
+     *
+     * @return тип животного
+     */
+    protected abstract AnimalType getType();
 
     /**
      * Возвращает породу животного. Должен быть имплементирован в классе-наследнике.
@@ -31,4 +44,11 @@ public abstract class AbstractAnimal implements Animal {
      * @return характер животного
      */
     protected abstract String getCharacter();
+
+    /**
+     * Возвращает кличку животного. Должен быть имплементирован в классе-наследнике.
+     *
+     * @return кличка животного
+     */
+    protected abstract String getName();
 }
--- src/main/java/ru/mtsb/okovalev/lessonthree/animals/Predator.java
+++ src/main/java/ru/mtsb/okovalev/lessonthree/animals/Predator.java
@@ -1,5 +1,7 @@
 package ru.mtsb.okovalev.lessonthree.animals;
 
+import ru.mtsb.okovalev.lessonthree.animals.enums.AnimalType;
+
 /**
  * Дикое животное, хищник – расширение базовой абстракции AbstractAnimal.
  */
@@ -7,11 +9,23 @@ public abstract class Predator extends AbstractAnimal {
     /**
      * Создаёт дикого хищника с указанными параметрами.
      *
+     * @param type      Тип
      * @param breed     Порода
      * @param character Характер
+     * @param name      Кличка
      */
-    public Predator(String breed, String character) {
-        super(breed, character);
+    public Predator(AnimalType type, String breed, String character, String name) {
+        super(type, breed, character, name);
+    }
+
+    /**
+     * Возвращает тип дикого хищника.
+     *
+     * @return тип дикого хищника
+     */
+    @Override
+    public AnimalType getType() {
+        return super.type;
     }
 
     /**
@@ -32,5 +46,30 @@ public abstract class Predator extends AbstractAnimal {
     @Override
     public String getCharacter() {
         return super.character;
+    }
+
+    /**
+     * Возвращает кличку дикого хищника.
+     *
+     * @return кличка дикого хищника
+     */
+    @Override
+    public String getName() {
+        return super.name;
+    }
+
+    /**
+     * Возвращает представление дикого животного в формате JSON.
+     *
+     * @return компактный (в одну строку) JSON, содержащий объект Predator со всеми полями класса Predator
+     */
+    @Override
+    public String toString() {
+        return "{"
+                + "\"type\":\"" + getType() + "\","
+                + "\"breed\":\"" + getBreed() + "\","
+                + "\"character\":\"" + getCharacter() + "\","
+                + "\"name\":\"" + getName() + "\""
+                + "}";
     }
 }
